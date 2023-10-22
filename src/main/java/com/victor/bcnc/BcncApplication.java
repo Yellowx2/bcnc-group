b54c5398f467446a5538ffaac3153eddb53b7f0f
Víctor Huang Huang <vic-16@hotmail.com>
2023-10-22 13:29:29 +0200

    Initial commit

---
+++ src/main/java/com/victor/bcnc/BcncApplication.java
@@ -0,0 +1,13 @@
+package com.victor.bcnc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcncApplication.class, args);
+	}
+
+}
